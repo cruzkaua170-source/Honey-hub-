92f09b673d0fe4805c8ff389240ba71bbf00039f
cruzkaua170-source <cruzkaua170@gmail.com>
2026-05-22 07:03:45 -0300

    Refactor multi-select handling and clean up code
    Refactor multi-select option handling and remove unused code.

--- main.lua
+++ main.lua
@@ -1,3 +1,16 @@
+					if MultiSelect then
+						local Stats = Selected[Name]
+						Selected[Name] = Stats or false
+						Options[Name].Stats = Stats
+					end
+					
+					local Button = Make("Button", ScrollFrame, {
+						Name = "Option",
+						Size = UDim2.new(1, 0, 0, 21),
+						Position = UDim2.new(0, 0, 0.5),
+						AnchorPoint = Vector2.new(0, 0.5)
+					})Make("Corner", Button, UDim.new(0, 4))
+					
 					local IsSelected = InsertTheme(Create("Frame", Button, {
 						Position = UDim2.new(0, 1, 0.5),
 						Size = UDim2.new(0, 4, 0, 4),
@@ -410,128 +423,5 @@
 	MinimizeButton.Activated:Connect(Window.MinimizeBtn)
 	return Window
 end
-
-return redzliblocal redzlib = loadstring(game:HttpGet(
-    "https da usa gui"))()
-
-local Window = redzlib:MakeWindow({
-    Title = "Honey-Hub",
-    SubTitle = "by Breno",
-    SaveFolder = "Honey.json",
-    Icon = "",
-    Key = "HONEY-8C2D9-4B5E7-6F1A3"
-})
-
-
-
-task.delay(0.1, function()
-    redzlib:SetBackgroundImage(135003123565230)
-end)
-
-  local Dialog = Window:Dialog({
-    Title = "Dialog",
-    Text = "This is a Dialog",
-    Options = {
-      {"Confirm", function()
-        
-      end},
-      {"Maybe", function()
-        
-      end},
-      {"Cancel", function()
-        
-      end}
-    }
-  })
-
-local minimizeIcon = "rbxassetid://71014873973869"
-pcall(function()
-    if writefile and getcustomasset then
-        if not isfile("honey_minimize.png") then
-            local success, content = pcall(game.HttpGet, game, "https://iili.io/C9E3NXn.png")
-            if success then
-                writefile("honey_minimize.png", content)
-            end
-        end
-        if isfile("honey_minimize.png") then
-            minimizeIcon = getcustomasset("honey_minimize.png")
-        end
-    end
-end)
-
-Window:AddMinimizeButton({
-    Button = { Image = minimizeIcon, BackgroundTransparency = 0, Size = UDim2.fromOffset(50, 20) },
-    Corner = { CornerRadius = UDim.new(100, 1) },
-})
-
-
-local Tab1 = Window:MakeTab({"teste", "cherry"})
-
-Tab1:AddDiscordInvite({
-    Name = "Honey Hub",
-    Description = "Join server",
-    Logo = "rbxassetid://18751483361",
-    Invite = "Link discord invite",
-})
-
-Tab1:AddSection({"Section"})
-
-Tab1:AddParagraph({"Paragraph", "This is a Paragraph\nSecond Line"})
-
-Tab1:AddButton({"Print", function(Value)
-print("Hello World!")
-end})
-
-local Toggle1 = Tab1:AddToggle({
-  Name = "Toggle",
-  Description = "This is a <font color='rgb(88, 101, 242)'>Toggle</font> Example",
-  Default = false 
-})
-Toggle1:Callback(function(Value)
- 
-end)
-
-Tab1:AddSlider({
-  Name = "Speed",
-  Min = 1,
-  Max = 100,
-  Increase = 1,
-  Default = 16,
-  Callback = function(Value)
-
-end
-})
-
-Tab1:AddDropdown({
-  Name = "Players List",
-  Description = "Select the <font color='rgb(88, 101, 242)'>Number</font>",
-  Options = {"one", "two", "three"},
-  Default = "two",
-  Flag = "dropdown teste",
-  Callback = function(Value)
-    
-  end
-})
-
-Tab1:AddTextBox({
-  Name = "Name item",
-  Description = "1 Item on 1 Server", 
-  PlaceholderText = "item only",
-  Callback = function(Value)
-end 
-})
-
--- [[ ESTE BLOCO ATIVA O SEU MENU APÓS AS 500 LINHAS ]]
-local Window = redzlib:MakeWindow({
-    Title = "Honey-Hub",
-    SubTitle = "by Breno",
-    SaveFolder = "Honey.json",
-    Key = "HONEY-8C2D9-4B5E7-6F1A3"
-})
-local Tab1 = Window:MakeTab({"Principal", "apple"})
-
-Tab1:AddButton({"Testar Script", function()
-    print("Honey Hub funcionando!")
-end})
 
 return redzlib
